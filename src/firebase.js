bf080f82ced86e3171066b02044e3eb17b9e217a
sureshsaravananbabu <50414410+sureshsaravananbabu@users.noreply.github.com>
2020-12-15 18:03:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,5 @@
 import firebase from "firebase"
-const firebaseConfig = {
-    apiKey: "AIzaSyAHO6b1cOLd0cNGKhbJY8T_yBlQj59IGDE",
-    authDomain: "facebook-clone-6cdb0.firebaseapp.com",
-    projectId: "facebook-clone-6cdb0",
-    storageBucket: "facebook-clone-6cdb0.appspot.com",
-    messagingSenderId: "1004824196400",
-    appId: "1:1004824196400:web:6214444bb03923e27204f5",
-    measurementId: "G-X04YFFKJWS"
-  };
+const firebaseConfig = {};
 
 const firebaseApp=firebase.initializeApp(firebaseConfig)
 const db=firebaseApp.firestore();
